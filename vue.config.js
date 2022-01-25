605a8d52d44f339e434febb35ac2c3101b1eb7b0
alfian an naufal nuha <alfianannaufalnuha@alfians-MacBook-Pro.local>
2022-01-25 10:46:03 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/face-detection-vue/'
-    : '/',
-  parallel: false,  // disable thread-loader, which is not compactible with this plugin
-  configureWebpack: {
-    plugins: [
-      require('unplugin-vue2-script-setup/webpack')({ /* options */ }),
-    ],
-  },
+    : '/'
 }
   
